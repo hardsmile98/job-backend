42300701a66251d79d0f1274a91ea8153a9e28da
Denis Kotelev <hardsmile98@mail.ru>
2020-10-14 21:02:28 +0300

    make it better

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/api/auth', auth)
 app.use('/api/job', job)
 app.use('/api/user', user)
 
-const PORT = config.get('port') || '5000'
+const PORT = process.env.PORT || '5000'
 async function start() {
   try {
     await mongoose.connect(config.get('mongoUrl'), {
