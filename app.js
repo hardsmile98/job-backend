45ea3508b826f3b32c925f8a96b5eb4abbe0226a
Denis Kotelev <hardsmile98@mail.ru>
2020-10-14 21:21:55 +0300

    make it better1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const config = require('config')
 const auth = require('./routes/auth')
 const job = require('./routes/job')
@@ -7,6 +8,7 @@ const user = require('./routes/user')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json({ extended: true }))
 
 app.get('/', (req, res) => {
